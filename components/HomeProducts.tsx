4974a379c39a4279c9506b0a0a514b8c2b719743
facuariasla <facundolautaroarias@hotmail.com>
2022-07-26 22:29:35 -0300

    styles hover on products and header added

--- components/HomeProducts.tsx
+++ components/HomeProducts.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import React from "react";
 import useStore from "../state_zustand";
 import styles from "../styles/Products.module.scss";
+import addImg from "../assets/addtocart.svg";
+
 const HomeProducts = () => {
   const inventory = useStore((state: any) => state.inventory);
   return (
@@ -11,7 +13,15 @@ const HomeProducts = () => {
         {inventory?.map((item: any) => (
           <div className={styles.product_card}>
             <div className={styles.image_container}>
-              <Image src={item.image} width={220} height={280} objectFit='cover'/>
+              <Image
+                src={item.image}
+                width={220}
+                height={280}
+                objectFit="cover"
+              />
+              <div className={styles.addtocart}>
+                <Image src={addImg} />
+              </div>
             </div>
             <div className={styles.card_data}>
               <p>{item.title}</p>
